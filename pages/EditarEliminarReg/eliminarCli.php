ed0db16ce465923a6af63fdc40ab5b85ae97d815
EstebanM <javier_esteban99@hotmail.com>
2021-08-15 00:23:35 -0500

    Revision del documento eliminar cliente

--- pages/EditarEliminarReg/eliminarCli.php
+++ pages/EditarEliminarReg/eliminarCli.php
@@ -1,14 +1,14 @@
 <?php
-    $user = "root";
-    $pass = "";
-    $host = "localhost";
-    $db = "lavado_autos";
+    $host    = "localhost";
+    $user    = "root";
+    $pass    = "";
+    $db_name = "lavado_autos";
 
     $con = mysqli_connect($host,$user,$pass);
-    mysqli_select_db ($con,$db);
+    mysqli_select_db ($con,$db_name);
 
     $id=$_GET['id'];
-    $eliminar = "DELETE FROM cliente WHERE cli_cedula='$id'";
+    $eliminar = "DELETE FROM CLIENTE WHERE cli_cedula='$id'";
     $resultado = mysqli_query($con,$eliminar);
 
     if($resultado){
